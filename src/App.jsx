45439bd02df64a6734470334a3bd227dd22e6575
Livea Brito <159591382+LiveaBrito@users.noreply.github.com>
2024-10-20 01:08:06 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Notas from './components/Notas'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Notas notaAluno={90}/>
+      <Notas notaAluno={75}/>
+      <Notas notaAluno={60}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Notas.jsx
@@ -0,0 +1,17 @@
+function Notas(props){
+
+    let nota = props.notaAluno
+
+    if(nota >= 90){
+        return <h1>Excelente!</h1>
+
+    }else if(nota >= 70 && nota <= 89){
+        return <h1>Bom</h1>
+    
+    }else{
+        return <h1>Precisa Melhorar</h1>
+    }
+
+}
+
+export default Notas
